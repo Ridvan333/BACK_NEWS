208aa2f474d1d64002330a320d35d5010955a19b
Shamil <shamil>
2022-03-14 18:20:40 +0300

    CCЫЛКА

--- index.js
+++ index.js
@@ -12,13 +12,13 @@ app.use(cors());
 
 async function connected() {
     try {
-        await mongoose.connected();
-    
-    console.log('Подключение к MongoDB прошло успешно');
-    app.listen(port, () => {
-        console.log('OK!');
-    })    
-    } catch(e) {
+        await mongoose.connected('mongodb+srv://mgls:12345@cluster0.od801.mongodb.net/test?authSource=admin&replicaSet=atlas-oy5gfc-shard-0&readPreference=primary&appname=MongoDB%20Compass&ssl=true');
+
+        console.log('Подключение к MongoDB прошло успешно');
+        app.listen(port, () => {
+            console.log('OK!');
+        })
+    } catch (e) {
         console.log('Не удалось подключиться');
     }
 }
